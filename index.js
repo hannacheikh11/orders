ebb42e48b4b1b0fb594046db9fe34e22188e8070
hannacheikh11 <hanna.cheikhmalaaynine@gmail.com>
2020-12-18 01:28:58 +0000

    add support to nebd for get  method

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
 var express= require('express');
 var bodyParser = require('body-parser');
+var DataStore = require ('nedb');
 var port =3000;
 var BASE_API_PATH="/api/V2";
-var orders=[
-    {"id":"1234" , "date":"20/12/2020"}
-];
+var  DB_FILE_NAME = __dirname + "/orders.json";
 console.log("Starting API server...");
 var app = express();
 app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
 app.get("/", (req,res) => {
   
-    res.send("<html><body><h>haxnna</h1></body></html>");
+    res.send("<html><body><h>haxnna</h1></body>npm</html>");
 
    
 
     });
     app.get(BASE_API_PATH + "/orders",(req,res)=>{
         console.log(Date() +"-GET /orders");
+
+        db.find({},(err,orders) => {
+           if (err){
+               console.log(Date() + "-"+ err);
+               res.sendStatus(500);
+           }else{
+
+        
         res.send(orders);
+           }
     });
+});
 
     app.post(BASE_API_PATH + "/orders",(req,res)=>{
         console.log(Date() +"-POST /orders");
 
 var order=req.body;
-orders.push(order);
-res.sendStatus(201);
+db.insert(order,(err) => {
+    if(err){
+        console.log(Date() + "-" + err);
+        res.sendStatus(500);
+    }else{
+        res.sendStatus(201);
+    }
+});
+
+
     });
 
         
