c2cd7fb4ae558182ed82d5b67a19ddc5faaad98b
hannacheikh11 <hanna.cheikhmalaaynine@gmail.com>
2021-01-05 19:20:51 +0000

    terminar los metodos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const Order = require('./orders');
+const { count } = require('./orders');
 
 var BASE_API_PATH = "/api/v2";
 
@@ -39,6 +40,40 @@ app.post(BASE_API_PATH + "/orders", (req, res) => {
         }
     });
 });
+
+// the put methode
+
+app.put(BASE_API_PATH +"/orders/_id", async (req, res) => {
+	// create mongose method to update a existing record into collection
+	let id = req.params._id;
+	var data = {
+		Name : req.body.Name,
+        DNI : req.body.DNI,
+        Adress : req.body.Adress,
+        email : req.body.email,
+       
+	}
+ 
+	// save the order
+	Order.findByIdAndUpdate(id, data, function(err, order) {
+	if (err) throw err;
+ 
+	res.send('Successfully! Order updated - ');
+	});
+});
 // the delete methode
+app.delete(BASE_API_PATH +"/orders/_id", async (req, res) => {
+    console.log(req.params._id);
+	let id = req.params._id;
+	Order.remove({
+		_id : id
+	}, function(err) {
+		if (err)
+			res.send(err);
+		else
+			res.send('Successfully! Order has been Deleted.');	
+	});
+});
+
  module.exports = app;
  
